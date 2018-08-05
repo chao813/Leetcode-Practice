d779fdbd6735c8ce20a4b56630ff3ee9d08ba8dc
Yuan Chao <yuanchao@Yuans-MacBook-Pro.local>
2018-08-05 15:43:20 -0400

    done easy

---
+++ 771JewelsandStones.py
@@ -0,0 +1,12 @@
+"""You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A"."""
+
+def numJewelsInStones(J, S):
+	J = list[J]
+	x = 0
+	for i in S:
+		if i in J:
+			x = x + 1
+
+	return x
