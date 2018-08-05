a1f5110d2dfc238b0c2365243406356e9ae9393b
Yuan Chao <yuanchao@Yuans-MacBook-Pro.local>
2018-08-05 16:13:38 -0400

    uses chr and ord

---
+++ 709Tolowercase.py
@@ -0,0 +1,11 @@
+"""given uppercase convert to lower case"""
+
+def lowercase(s):
+	ans = []
+    for i in str:
+        if (65 <= ord(i) <= 90):
+            ans.append(chr(ord(i)+32))
+        else:
+            ans.append(i)
+
+    return("".join(ans))
