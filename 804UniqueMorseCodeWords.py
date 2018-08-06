bf84da86b94d1b2138dee9f76e8e25915b9b2800
Yuan Chao <yuanchao@Yuans-MacBook-Pro.local>
2018-08-05 21:01:26 -0400

    easy use of ord

---
+++ 804UniqueMorseCodeWords.py
@@ -0,0 +1,21 @@
+"""International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have."""
+
+def uniqueMorseRepresentations(words):
+	lists = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    answer = []
+    for word in words:
+        temp = ""
+        for letter in word:
+            index = ord(letter)-97
+            temp = temp + lists[index]
+        answer.append(temp)
+    print(answer)
+    answer = set(answer)
+    return(len(answer))
