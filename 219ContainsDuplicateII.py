372d3054d85cc3db8c155d153048f9c4f4eb07a3
chao813 <chao9@purdue.edu>
2018-08-23 15:22:00 -0400

    uses dictionary, revisit later

---
+++ 219ContainsDuplicateII.py
@@ -0,0 +1,33 @@
+"""Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false"""
+
+def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        if len(nums)<2:
+            return False
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]] = i
+            else:
+                if i - dic[nums[i]] <= k:
+                    return True
+                else:
+                    dic[nums[i]] = i
+        return False
